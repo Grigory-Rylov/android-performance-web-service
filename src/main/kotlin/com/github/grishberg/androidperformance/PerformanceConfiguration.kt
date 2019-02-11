33927a2dfde175f191ef6280a2bdba13a9e3500e
Grigory Rylov <grishberg@gmail.com>
2019-02-11 23:01:13 +0300

    syntax checker

--- src/main/kotlin/com/github/grishberg/androidperformance/PerformanceConfiguration.kt
+++ src/main/kotlin/com/github/grishberg/androidperformance/PerformanceConfiguration.kt
@@ -4,7 +4,11 @@ data class PerformanceConfiguration(
         val sourceType1: String? = null,
         val sourceType2: String? = null,
         val import1: String? = null,
+        val init1: String? = null,
+        val fields1: String? = null,
         val import2: String? = null,
+        val init2: String? = null,
+        val fields2: String? = null,
         val source1: String? = null,
         val source2: String? = null,
         val results: ArrayList<PerfomanceResults> = ArrayList()
